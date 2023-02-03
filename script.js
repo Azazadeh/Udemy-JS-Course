f94ad362dc057aade2deecc8b1be108a77e09f89
Azazadeh <azadeh.nourbakhsh@gmail.com>
2023-02-03 22:07:20 +0300

    Bonus 1 & 2 added. Challenge #3 Done

--- script.js
+++ script.js
@@ -22,16 +22,44 @@ if (markBmi > johnBmi) {
 
 //Coding Challenge #3
 
-const dolphinesAverageScore = (96 + 108 + 89) / 3;
+// const dolphinesAverageScore = (96 + 108 + 89) / 3;
+// console.log(dolphinesAverageScore);
+
+// const koalasAverageScore = (88 + 91 + 110) / 3;
+// console.log(koalasAverageScore);
+
+// if (dolphinesAverageScore > koalasAverageScore) {
+//   console.log("Dolphines Are the winners!");
+// } else if (dolphinesAverageScore < koalasAverageScore) {
+//   console.log("Koalas are the winners!");
+// } else {
+//   console.log("It's a tie!");
+// }
+
+//Bonus 1 and 2
+
+const dolphinesAverageScore = (97 + 112 + 101) / 3;
 console.log(dolphinesAverageScore);
 
-const koalasAverageScore = (88 + 91 + 110) / 3;
+const koalasAverageScore = (109 + 95 + 123) / 3;
 console.log(koalasAverageScore);
 
-if (dolphinesAverageScore > koalasAverageScore) {
+if (
+  dolphinesAverageScore > koalasAverageScore &&
+  dolphinesAverageScore >= 100
+) {
   console.log("Dolphines Are the winners!");
-} else if (dolphinesAverageScore < koalasAverageScore) {
+} else if (
+  dolphinesAverageScore < koalasAverageScore &&
+  koalasAverageScore >= 100
+) {
   console.log("Koalas are the winners!");
+} else if (
+  dolphinesAverageScore === koalasAverageScore &&
+  koalasAverageScore >= 100 &&
+  dolphinesAverageScore >= 100
+) {
+  console.log("Both teams are winners!");
 } else {
-  console.log("It's a tie!");
+  console.log("No team wins!");
 }
